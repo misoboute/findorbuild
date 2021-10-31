dae5e8b822b6916b08956ffab6c40b4fa8a868dc
Muhammad Ismail Soboute <misoboute@gmail.com>
2021-10-31 23:23:26 +0100

    GTest retrieve module will support later versions

--- cmake/FOB-Retrieve-GTest.cmake
+++ cmake/FOB-Retrieve-GTest.cmake
@@ -8,7 +8,9 @@ set(GTEST_VERSIONS
     "1.6.0" "1.7.0" "1.8.0" "1.8.1" "1.10.0" "1.11.0" 
 )
 
-fob_set_default_var_value(FOB_REQUESTED_VERSION 1.11.0)
+list(GET GTEST_VERSIONS -1 GTEST_LATEST_VERSION)
+
+fob_set_default_var_value(FOB_REQUESTED_VERSION ${GTEST_LATEST_VERSION})
 fob_set_default_var_value(BUILD_SHARED_LIBS OFF)
 fob_set_default_var_value(gtest_force_shared_crt OFF)
 
@@ -16,10 +18,11 @@ fob_normalize_version_number(FOB_REQUESTED_VERSION)
 string(REGEX REPLACE "\\.[0-9]$" "" 
     FOB_REQUESTED_VERSION ${FOB_REQUESTED_VERSION})
 
-if(FOB_REQUESTED_VERSION IN_LIST GTEST_VERSIONS)
+if(FOB_REQUESTED_VERSION IN_LIST GTEST_VERSIONS OR 
+    FOB_REQUESTED_VERSION VERSION_GREATER GTEST_LATEST_VERSION)
     set(VERSION_GIT_TAG release-${FOB_REQUESTED_VERSION})
 else()
-    set(VERSION_GIT_TAG release-1.11.0)
+    set(VERSION_GIT_TAG release-${GTEST_LATEST_VERSION})
 endif()
 
 fob_add_ext_cmake_project(
