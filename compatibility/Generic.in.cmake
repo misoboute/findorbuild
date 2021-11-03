982a0d2c6d94c67e3d963618fd236d66fc76bc82
Muhammad Ismail Soboute <misoboute@gmail.com>
2021-11-03 01:52:15 +0100

    WIP Initial implementation of generic config compatibility checks

--- compatibility/Generic.in.cmake
+++ compatibility/Generic.in.cmake
@@ -12,21 +12,75 @@ if(NOT CMAKE_SIZEOF_VOID_P EQUAL @CMAKE_SIZEOF_VOID_P@)
     return()
 endif()
 
-# C compiler binary compatibility test - TODO (CMAKE_C_COMPILER_ID)
+# Sets of mutually binary-compatible compilers based on OS type
+if(APPLE)
+    set(BINARY_COMPATIBLE_COMPILERS_1 AppleClang GNU)
+    set(BINARY_COMPATIBLE_COMPILERS_2 GHS)
+    set(NUM_BINARY_COMPATIBLE_COMPILER_SETS 2)
+elseif(WIN32)
+    set(BINARY_COMPATIBLE_COMPILERS_1
+        Clang GNU Intel IntelLLVM OpenWatcom PathScale PGI)
+    set(BINARY_COMPATIBLE_COMPILERS_2 MSVC)
+    set(BINARY_COMPATIBLE_COMPILERS_3 Embarcadero Borland)
+    set(BINARY_COMPATIBLE_COMPILERS_4 GHS)
+    set(BINARY_COMPATIBLE_COMPILERS_5 XL XLClang)
+    set(NUM_BINARY_COMPATIBLE_COMPILER_SETS 5)
+elseif(UNIX)
+    set(BINARY_COMPATIBLE_COMPILERS_1
+        Clang GNU Intel IntelLLVM OpenWatcom PathScale PGI)
+    set(BINARY_COMPATIBLE_COMPILERS_2 GHS)
+    set(BINARY_COMPATIBLE_COMPILERS_3 XL XLClang)
+    set(BINARY_COMPATIBLE_COMPILERS_4 SunPro)
+    set(NUM_BINARY_COMPATIBLE_COMPILER_SETS 4)
+endif()
 
-# C++ compiler binary compatibility test - TODO (CMAKE_CXX_COMPILER_ID)
+# Add singular sets that could (might?) be used with all OS types
+foreach(COMPILER_SET Bruce "Fujitsu;FujitsuClang" HP IAR SDCC TinyCC)
+    math(EXPR NUM_BINARY_COMPATIBLE_COMPILER_SETS 
+        "NUM_BINARY_COMPATIBLE_COMPILER_SETS + 1")
+    set(BINARY_COMPATIBLE_COMPILERS_${NUM_BINARY_COMPATIBLE_COMPILER_SETS}
+        ${COMPILER_SET})
+endforeach(COMPILER_SET)
 
-# Host system test - TODO (CMAKE_SYSTEM_NAME)
+# C compiler binary compatibility test
+foreach(CNT RANGE 1 ${NUM_BINARY_COMPATIBLE_COMPILER_SETS})
+    set(COMPATIBLE_SET ${BINARY_COMPATIBLE_COMPILERS_${CNT}})
+    if(CMAKE_C_COMPILER_ID IN_LIST COMPATIBLE_SET)
+        if(NOT @CMAKE_C_COMPILER_ID@ IN_LIST BINARY_COMPATIBLE_COMPILERS)
+            set(FOB_IS_COMPATIBLE false)
+            return()
+        endif()
+        break()
+    endif()
+endforeach(CNT)
 
-# Test CMAKE_OSX_DEPLOYMENT_TARGET compatibility for apple
+# C++ compiler binary compatibility test
+foreach(CNT RANGE 1 ${NUM_BINARY_COMPATIBLE_COMPILER_SETS})
+    set(COMPATIBLE_SET ${BINARY_COMPATIBLE_COMPILERS_${CNT}})
+    if(CMAKE_CXX_COMPILER_ID IN_LIST COMPATIBLE_SET)
+        if(NOT @CMAKE_CXX_COMPILER_ID@ IN_LIST BINARY_COMPATIBLE_COMPILERS)
+            set(FOB_IS_COMPATIBLE false)
+            return()
+        endif()
+        break()
+    endif()
+endforeach(CNT)
+
+# Test target OS compatibility
+if(NOT CMAKE_SYSTEM_NAME STREQUAL @CMAKE_SYSTEM_NAME@)
+    set(FOB_IS_COMPATIBLE false)
+    return()
+endif()
 
 if(XCODE)
+    # Test CMAKE_OSX_DEPLOYMENT_TARGET compatibility
     if(NOT XCODE_VERSION VERSION_EQUAL @XCODE_VERSION@ OR
         NOT CMAKE_OSX_DEPLOYMENT_TARGET VERSION_EQUAL @CMAKE_OSX_DEPLOYMENT_TARGET@)
         set(FOB_IS_COMPATIBLE false)
         return()
     endif()
 elseif(MSVC)
+    # Test MSVC toolset version compatibility
     list(APPEND BUILD_DISTINGUISHING_VARS MSVC_TOOLSET_VERSION)
     if(NOT MSVC_TOOLSET_VERSION VERSION_EQUAL @MSVC_TOOLSET_VERSION@)
         set(FOB_IS_COMPATIBLE false)
